bb1f7adb4e94de47a1c3043c022673ce4b801a26
vpellegrino <vpellegr@redhat.com>
2022-01-12 19:47:10 +0100

    KOGITO-4588: DataType concept in the editor should cover both "custom" and "out-of-the-box" data types

--- packages/stunner-editors-dmn-loader/src/index.tsx
+++ packages/stunner-editors-dmn-loader/src/index.tsx
@@ -18,6 +18,7 @@ import {
   BoxedExpressionEditor,
   BoxedExpressionEditorProps,
   ContextProps,
+  DataTypeProps,
   DecisionTableProps,
   ExpressionProps,
   FunctionProps,
@@ -35,6 +36,7 @@ import * as ReactDOM from "react-dom";
 const BoxedExpressionWrapper: React.FunctionComponent<BoxedExpressionEditorProps> = ({
   decisionNodeId,
   expressionDefinition,
+  dataTypes,
   clearSupportedOnRootExpression,
   pmmlParams,
 }: BoxedExpressionEditorProps) => {
@@ -91,6 +93,7 @@ const BoxedExpressionWrapper: React.FunctionComponent<BoxedExpressionEditorProps
     <BoxedExpressionEditor
       decisionNodeId={decisionNodeId}
       expressionDefinition={updatedDefinition}
+      dataTypes={dataTypes}
       clearSupportedOnRootExpression={clearSupportedOnRootExpression}
       pmmlParams={pmmlParams}
     />
@@ -101,6 +104,7 @@ const renderBoxedExpressionEditor = (
   selector: string,
   decisionNodeId: string,
   expressionDefinition: ExpressionProps,
+  dataTypes: DataTypeProps[],
   clearSupportedOnRootExpression: boolean,
   pmmlParams: PMMLParams
 ) => {
@@ -108,6 +112,7 @@ const renderBoxedExpressionEditor = (
     <BoxedExpressionWrapper
       decisionNodeId={decisionNodeId}
       expressionDefinition={expressionDefinition}
+      dataTypes={dataTypes}
       clearSupportedOnRootExpression={clearSupportedOnRootExpression}
       pmmlParams={pmmlParams}
     />,
